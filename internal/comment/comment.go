400c1f2b86f46360027f795b9024e7e41bb286a7
Galbeyte <s.galbeyte@gmail.com>
2023-07-18 13:19:09 -0400

    Implementing the new Comment Service using gorm methods on the ORM. Removing CreatedAt attribute as it may be populated/detailed by the Save method from the gorm model.

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -1,8 +1,6 @@
 package comment
 
 import (
-	"time"
-
 	"github.com/jinzhu/gorm"
 )
 
@@ -13,11 +11,12 @@ type Service struct {
 
 // Defines our comment structure
 type Comment struct {
+	// id, created time and updated time will be populated from gorm by default (supposedly)
 	gorm.Model 		// Tell GORM this will be structured in the DB
 	Slug string		// Represents the path where this comment is posted
 	Body string		// Conetent of the comment
 	Author string
-	CreatedAt time.Time
+	// CreatedAt time.Time
 }
 
 // CommentService - the interface for our comment service
@@ -37,3 +36,60 @@ func NewService(db *gorm.DB) *Service {
 	}
 }
 
+// GetComment - retrieves comments by their ID from the database
+func (s *Service) GetComment(ID uint) (Comment, error) {
+	var comment Comment
+	if result := s.DB.First(&comment, ID); result.Error != nil {
+		return Comment{}, result.Error
+	}
+	return comment, nil
+}
+
+// GetCommentsBySlug - retrieves all comments by slug (path - /article/name/)
+func (s *Service) GetCommentsBySlug(slug string) ([]Comment, error) {
+	var comments []Comment
+	if result := s.DB.Find(&comments).Where("slug = ?", slug); result.Error != nil {
+		return []Comments{}, result.Error
+	}
+
+	return comments, nil
+}
+
+//
+func (s *Service) PostComment(comment Comment) (Comment, error) {
+	if result := s.DB.Save(&comment); result.Error != nil {
+		return Comment{}, result.Error
+	}
+	return comment, nil
+}
+
+// UpdateComment - updated a comment by ID with a new comment info
+func (s *Service) UpdateComment(ID uint, newComment Comment) (Comment, error) {
+	comment, err := s.GetComment(ID)
+	if err != nil {
+		return Comment{}, err
+	}
+
+	if result := s.DB.Model(&comment).Updates(newComment); result.Error != nil {
+		return Comment{}, result.Error
+	}
+
+	return comment, err
+}
+
+// DeleteComment - deletes a comment from the database by ID
+func (s *Service) DeleteComment(ID uint) error {
+	if result := s.DB.Delete(&Comment{}, ID); result.Error != nil {
+		return result.Error
+	}
+	return nil
+}
+
+// GetAllComments - retrieves all comments from the database
+func (s *Service) GetAllComments() ([]Comment, error) {
+	var comments []Comment
+	if result := s.DB.Find(&comments); result.Error != nil {
+		return comments, result.Error
+	}
+	return comments, nil
+}
